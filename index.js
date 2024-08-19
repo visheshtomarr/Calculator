447ad4b5d2e66b7f08d74aa26baa326a13d16db1
visheshtomarr <visheshtomar7800@gmail.com>
2024-08-19 16:21:52 +0530

    Added documentation for index.js

--- index.js
+++ index.js
@@ -2,14 +2,17 @@
 
 const display = document.querySelector('#display') ;
 
+// Function to display the input string.
 function appendToDisplay(inputForDisplay) {
     display.value += inputForDisplay ;
 }
 
+// Function will clear the input string.
 function clearCalculatorDisplay() {
     display.value = '' ;
 }
 
+// Function will return error if any wrong mathematical expression is given as input.
 function calculate() {
     try {
         display.value = eval(display.value) ;
