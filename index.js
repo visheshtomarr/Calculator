d3f1b8e3b7a3a2ca20867a80546de422f8c0c267
visheshtomarr <visheshtomar7800@gmail.com>
2024-08-19 15:57:35 +0530

    Refactored code and implemented calculate() function

--- index.js
+++ index.js
@@ -9,3 +9,12 @@ function appendToDisplay(inputForDisplay) {
 function clearCalculatorDisplay() {
     display.value = '' ;
 }
+
+function calculate() {
+    try {
+        display.value = eval(display.value) ;
+    }
+    catch(error) {
+        display.value = 'Error' ;
+    }
+}
